3ae011bf777f144be067c5d5210098fb924f7adc
crew852 <crew852@naver.com>
2023-10-08 00:24:21 +0900

    Update pcap.c

--- pcap.c
+++ pcap.c
@@ -25,28 +25,31 @@ void packet_processor(const struct pcap_pkthdr* header, const u_char* packet) {
     u_short tcp_source_port = *((u_short*)tcp_header);
     u_short tcp_dest_port = *((u_short*)(tcp_header + 2));
 
-    // TCP 의 Offset 필드를 이용해 Payload의 시작점을 찾고 패킷 전체길이(header->len)에서 Payload 시작점(payload - packet)을 빼서 길이를 구한다
-    u_char* payload = tcp_header + ((tcp_header[12] >> 4) * 4);
-    int payload_length = header->len - (payload - packet);
+    // TCP 프로토콜 번호를 확인하여 TCP 패킷인 경우에만 출력
+    if (ip_header[9] == 6) {  // TCP 프로토콜 번호 = 6
 
-    printf("[2계층] Src MAC: %02x:%02x:%02x:%02x:%02x:%02x, Dst MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
-           eth_source[0], eth_source[1], eth_source[2], eth_source[3], eth_source[4], eth_source[5],
-           eth_dest[0], eth_dest[1], eth_dest[2], eth_dest[3], eth_dest[4], eth_dest[5]);
+        // TCP 의 Offset 필드를 이용해 Payload의 시작점을 찾고 패킷 전체길이(header->len)에서 Payload 시작점(payload - packet)을 빼서 길이를 구한다
+        u_char* payload = tcp_header + ((tcp_header[12] >> 4) * 4);
+        int payload_length = header->len - (payload - packet);
 
-    printf("[3계층] Src IP: %u.%u.%u.%u, Dst IP: %u.%u.%u.%u\n",
-           ip_source[0], ip_source[1], ip_source[2], ip_source[3],
-           ip_dest[0], ip_dest[1], ip_dest[2], ip_dest[3]);
+        printf("[2계층] Src MAC: %02x:%02x:%02x:%02x:%02x:%02x, Dst MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+               eth_source[0], eth_source[1], eth_source[2], eth_source[3], eth_source[4], eth_source[5],
+               eth_dest[0], eth_dest[1], eth_dest[2], eth_dest[3], eth_dest[4], eth_dest[5]);
 
-    printf("[4계층] Src Port: %d, Dst Port: %d\n", ntohs(tcp_source_port), ntohs(tcp_dest_port));
+        printf("[3계층] Src IP: %u.%u.%u.%u, Dst IP: %u.%u.%u.%u\n",
+               ip_source[0], ip_source[1], ip_source[2], ip_source[3],
+               ip_dest[0], ip_dest[1], ip_dest[2], ip_dest[3]);
 
-    printf("Payload (최대 16 byte): ");
-    for (int i = 0; i < payload_length && i < 16; i++) {
-        printf("%02x ", payload[i]);
+        printf("[4계층] Src Port: %d, Dst Port: %d\n", ntohs(tcp_source_port), ntohs(tcp_dest_port));
+
+        printf("Payload (최대 16 byte): ");
+        for (int i = 0; i < payload_length && i < 16; i++) {
+            printf("%02x ", payload[i]);
+        }
+        printf("\n");
+
+        printf("패킷의 전체 길이: %u\n\n", header->len);
     }
-    printf("\n");
-
-    printf("패킷의 전체 길이: %u\n\n", header->len);
-
 }
 
 int main(int argc, char* argv[]) {
